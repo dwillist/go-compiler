b34471fcdad6f54f70cd1b0e86fa8c7e148d11d9
Daniel Thornton <dthornton@pivotal.io>
2020-04-22 10:23:32 -0400

    Fetch right before describing tag

--- .github/util/tag.sh
+++ .github/util/tag.sh
@@ -4,6 +4,8 @@ set -eu
 set -o pipefail
 
 function main() {
+  git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+
   local previous tag
   previous="$(git describe --abbrev=0 --tags)"
   tag="$(printf "%s" "$previous" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
