5d4afe3ea887d62532ed87b4ab46c3542aa69c1d
Daniel Thornton <dthornton@pivotal.io>
2020-04-22 10:25:41 -0400

    try diff git command

--- .github/util/tag.sh
+++ .github/util/tag.sh
@@ -4,10 +4,11 @@ set -eu
 set -o pipefail
 
 function main() {
-  git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+ # git fetch --depth=1 origin +refs/tags/*:refs/tags/*
 
   local previous tag
-  previous="$(git describe --abbrev=0 --tags)"
+ # previous="$(git describe --abbrev=0 --tags)"
+  previous="$(git describe --tags `git rev-list --tags --max-count=1`)"
   tag="$(printf "%s" "$previous" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')"
 
   printf "v%s" "${tag#v}"
